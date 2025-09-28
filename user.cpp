fd35952dd55968a7f222339a9812e8fc6c1e7014
unknown <abidmmu369@gmail.com>
2025-09-28 21:27:23 +0800

    Merged all files into the whole programme, progressing to testing code

--- user.cpp
+++ user.cpp
@@ -1,38 +1,156 @@
 #include<iostream>
 #include<iomanip>
+#include<fstream>
+#include<sstream>
+#include<vector>
+#include<string>
 #include"user.h"
 using namespace std;
 
-
 bool User::checkid(const string &id) 
 {
+    cin.clear();
+    fstream fin;
+    fin.open(filename, ios::in);
+    
+    if(!fin.is_open()) {
+        return false;
+    }
 
+    vector<string> row;
+    string line, word;
+
+    // Skip header
+    getline(fin, line);
+
+    while(getline(fin, line)) {
+        stringstream s(line);
+        row.clear();
+        
+        while(getline(s, word, ',')) {
+            row.push_back(word);
+        }
+        
+        if(row[0] == id) {
+            this->id = id;
+            this->password = row[1];
+            this->email = row[2];
+            this->address = row[3];
+            fin.close();
+            return true;
+        }
+    }
+    fin.close();
+    return false;
 }
 
 bool User::registerID(string &username)
 {
+    cin.clear();
+    // Check if username already exists
+    if(checkid(username)) {
+        return false;
+    }
 
+    // If username is unique, save it
+    this->id = username;
+    cout << "Enter email: ";
+    getline(cin, this->email);
+    cout << "Enter address: ";
+    getline(cin, this->address);
+    cout << "Enter password: ";
+    string pass;
+    getline(cin, pass);
+    this->password = pass;
+
+    // Save to file
+    ofstream fout;
+    fout.open(filename, ios::app);
+    if(!fout.is_open()) {
+        cout << "Error: Could not open file to register user\n";
+        return false;
+    }
+
+    // Write new user data
+    fout << id << "," << password << "," << email << "," << address << endl;
+    fout.close();
+    return true;
 }
-
 
 void User::updatePassword(const string &pass)
 {
+    cin.clear();
+    this->password = pass;
+    
+    // Read all records
+    vector<vector<string>> records;
+    fstream fin;
+    fin.open(filename, ios::in);
+    string line, word;
+    
+    // Store header
+    getline(fin, line);
+    records.push_back({line});
+    
+    while(getline(fin, line)) {
+        stringstream s(line);
+        vector<string> row;
+        while(getline(s, word, ',')) {
+            row.push_back(word);
+        }
+        if(row[0] == this->id) {
+            row[1] = pass; // Update password
+        }
+        records.push_back(row);
+    }
+    fin.close();
 
+    // Write updated records back
+    ofstream fout;
+    fout.open(filename, ios::out);
+    for(const auto &record : records) {
+        for(size_t i = 0; i < record.size(); i++) {
+            fout << record[i];
+            if(i < record.size() - 1) fout << ",";
+        }
+        fout << endl;
+    }
+    fout.close();
 }
+
 bool User::checkPassword(const string &password)
 {
-
+    return this->password == password;
 }
 
 string User::getName()
 {
-
+    return this->id;
 }
 
 string User::getAddress()
 {
-
+    return this->address;
 }
+
+
+/*
+
+bool User::checkid(const string &id) 
+{}
+bool User::registerID(string &username)
+{}
+
+void User::updatePassword(const string &pass)
+{}
+bool User::checkPassword(const string &password)
+{}
+
+string User::getName()
+{}
+
+string User::getAddress()
+{}*/
 
 
 
--- userInterface.cpp
+++ userInterface.cpp
@@ -11,6 +11,7 @@
 using namespace std;
 
 void showMenu();
+int showCartOption();
 
 
 int main()
@@ -40,16 +41,62 @@ int main()
     //menu that is shown to user!
     do
     {
+        int cart_option=0 , listing_id=0 , newQty=0 ;
+        char choice;
         showMenu();
         cin>>user_choice;
         switch(user_choice)
         {
         case 1:
-            cout<<"calling cart module!"<<endl;
-            cout<<"Cart module feautures: delete prodcut, change quality ,CLEAR CART, see sumarry at the end. Inside: save cart to user file\n";
-            break;
+            save_place.showCartSummary();
+            cart_option= showCartOption();
+        do{
+                switch(cart_option) 
+                {
+                case 1:
+                    cout << "Enter listing ID to delete.\n";
+                    cin>>listing_id;
+                     if (listing_id < 0) 
+                      cout << "Invalid ID.\n";
+                    else
+                        save_place.deleteProductfromCart(listing_id);
+                    break;
+
+                case 2:
+                    cout << "Enter listing ID and new quantity.\n";
+                    cin>>listing_id>>newQty;
+                    if (newQty < 0 || listing_id<0) 
+                         cout << "Invalid number!\n";
+                    else
+                        save_place.updateQuantityinCart(listing_id,newQty);
+                    break;
+                case 3:
+                    cout << "Showing cart summary...\n";
+                    save_place.showCartSummary();
+                    break;
+
+                case 4:
+                    cout << "Clear cart? (y/n)\n";
+                    cout<<"##Notice: Once cart is deleted, It can't be undone!\n";
+                    cin>>choice;
+                    if(choice =='y')
+                        save_place.clearCart();
+                    else
+                        cout<<"Retaining cart!\n";
+                    break;
+
+                case 0:
+                    cout << "Saving cart and returning to main menu...\n";
+                    break;
+
+                default:
+                    cout << "Invalid option. Try again.\n";
+                }
+            showCartOption();
+        }while(cart_option!=0);
+        break;
         case 2:
-            cout<<"calling checkout module!"<<endl;
+            cout<<"calling checkout module..."<<endl;
             cout<<"Logistics under construction ! Please stick to enjoy seamless shopping with us \n $$ We're luancing soon!! \n";
             break;
         case 3:
@@ -57,7 +104,9 @@ int main()
             cout<<"\n";
             break;
         case 0:
-            cout<<"Logging out..!Bye!\n";
+            cout << "Saving your cart before logout..." << endl;
+            save_place.generateCartfile();   // writes cart to file
+             cout << "Goodbye!" << endl;
             break;
         default:
             cout<<"Invalid option!! Please select correct menu\n";
@@ -77,5 +126,28 @@ void showMenu()
     cout<<"Press 3 to search product\n";
     cout<<"Press 0 to logout\n";
 }
+
+
+    int showCartOption()
+{
+    int option;
+    cout << "\n\n=========== Cart Menu ===========" << endl;
+    cout << "Press 1 to delete a product from cart" << endl;
+    cout << "Press 2 to update product quantity" << endl;
+    cout << "Press 3 to view cart summary" << endl;
+    cout << "Press 4 to clear the cart" << endl;
+    cout << "Press 0 to go back to main menu" << endl;
+    cout << "================================" << endl;
+    cin>>option;
+    if(option>=0 && option<=4)
+        return option;
+    else
+    {
+        cout<<"Invalid option!\n";
+        return;
+    }
+}
+
+    
 
 
