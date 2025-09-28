70d6f31542f967e53193c87b22c10dc6a48a7ebd
Nadota <atripa235@gmail.com>
2025-09-28 13:06:23 +0800

    update

--- Cart.cpp
+++ Cart.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-
-
+#include <iomanip>
+#include <map>
+#include "Cart.h"
 
 using namespace std;
+
+Cart::Cart(const string& currentUsername) {
+    username = currentUsername;
+    Cartfilename = "cart_" + username + ".csv";
+
+    loadSourceProductList("product.csv", "listing.csv");
+
+    loadCartfromFile();
+}
+
+void Cart::loadSourceProductList(const string& Productfile, const string& Filelisting) {
+    map <int, string> Nameofproduct;
+    ifstream Filep(Productfile);
+    if (Filep.is_open()) {
+        string line;
+        getline(Filep, line);
+        while (getline(Filep, line)) {
+            
+        }
+    }
+};
