67eb32d2694095ae40b312d45c98c1a0a7574c45
AbidTheWeber <abidmmu369@gmail.com>
2025-09-28 11:32:01 +0800

    odan finished cart.h

--- Cart.h
+++ Cart.h
@@ -1,6 +1,54 @@
-#ifndef CART_HPP
-#define CART_HPP
+#ifndef CART_H
+#define CART_H
 
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct DetailsofProduct 
+{
+    int Idlisting;
+    int Idproduct;
+    string Nameofproduct;
+    string Nameofseller;
+    double price;
+    int Stockavailable;
+};
+
+struct Itemcart 
+{
+    DetailsofProduct product;
+    int quantity;
+};
+
+class Cart 
+{
+private:
+    string username;
+    string Cartfilename;
+    vector<Itemcart> items;
+    vector<DetailsofProduct> sourceProductList;
+
+    void loadSourceProductList(const string& Productflie, const string& Filelisting);
+
+    void loadCartfromFile();
+
+public: 
+    Cart(const string& currentUsername);
+
+    void addProductTocart (int Idlisting, int quantity);
+
+    void deleteProductfromCart (int Idlisting);
+    
+    void clearCart();
+
+    void showCartSummary();
+
+    void updateQuantityinCart(int Idlisting, int newQuantity);
+
+    void generateCartfile();
+};
 
 
 #endif
