7f1e5755e530745717afa6c932840faad8148259
Nadota <atripa235@gmail.com>
2025-09-28 10:11:38 +0800

    Update Cart.h

---
+++ Cart.h
@@ -0,0 +1,46 @@
+#ifndef CART_H
+#define CART_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct DetailsofProduct 
+{
+    int Idlisting;
+    int Idproduct;
+    string Nameofproduct;
+    string Nameofseller;
+    double price;
+    int Stockavailable;
+};
+
+struct Itemcart 
+{
+    DetailsofProduct product;
+    int quantity;
+};
+
+class Cart 
+{
+private:
+    string username;
+    string Cartfilename;
+    vector<Itemcart> items;
+    vector<DetailsofProduct> sourceProductList;
+
+    void loadSourceProductList(const string& Productflie, const string& Filelisting);
+
+    void loadCartfromFile();
+
+public: 
+    Cart(const string& currentUsername);
+
+    void addProductTocart (int Idlisting, int quantity);
+
+    
+};
+
+
+#endif
